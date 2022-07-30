913cf45d1d372838b60a5378667858682f886f66
Anna <107642838+Kashitsina@users.noreply.github.com>
2022-07-30 19:13:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+void Zadacha65()
+{
+  // Задайте значение M и N.напишите программу ,которая выведет все натуральные числа в промежутке от М до N
+
+
+  int numberN = 5 ;
+  int numberM = 10;
+
+  Recursion(numberN , numberM);
+  Console.WriteLine();
+
+
+  void Recursion(int numberN, int numberM)
+  {
+    if (numberN == numberM - 1 ) return ;
+     
+    Console.Write(numberN +1 + "  ");
+    numberN ++;
+    Recursion( numberN, numberM);
+    
+  }
+   
+  
+
+}
+Zadacha65();
